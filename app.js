79b121116d3c64c2d9f48fb0833bf62a4e5077c1
Viki Zieleniewska <viktoriya.zieleniewska@gmail.com>
2023-05-30 11:08:33 +0200

    promise using kebab example, remember kebab joke

--- app.js
+++ app.js
@@ -1,24 +1,44 @@
 const boxRef = document.querySelector(".box");
 
-let isFinished = false;
+// let isFinished = false;
 
-// callback hell
-setTimeout(() => {
-  boxRef.style.left = "500px";
+// // callback hell
+// setTimeout(() => {
+//   boxRef.style.left = "500px";
+//   setTimeout(() => {
+//     boxRef.style.top = "500px";
+//     setTimeout(() => {
+//       boxRef.style.left = "0px";
+//       setTimeout(() => {
+//         boxRef.style.top = "0px";
+//         isFinished = true;
+//       }, 3000);
+//     }, 3000);
+//   }, 3000);
+// }, 3000);
+
+// setTimeout(() => {
+//   if (isFinished) {
+//     console.log("Run again");
+//   }
+// }, 15000);
+
+const receipt = new Promise((resolve, reject) => {
   setTimeout(() => {
-    boxRef.style.top = "500px";
-    setTimeout(() => {
-      boxRef.style.left = "0px";
-      setTimeout(() => {
-        boxRef.style.top = "0px";
-        isFinished = true;
-      }, 3000);
-    }, 3000);
-  }, 3000);
-}, 3000);
+    resolve("Kebab my friend!");
+  }, 3001);
 
-setTimeout(() => {
-  if (isFinished) {
-    console.log("Run again");
-  }
-}, 15000);
+  setTimeout(() => {
+    reject("No kebab, wanna fight?");
+  }, 3000);
+});
+
+console.log(receipt);
+
+receipt
+  .then((msg) => {
+    console.log(msg);
+  })
+  .catch((msg) => {
+    console.log(msg);
+  });
